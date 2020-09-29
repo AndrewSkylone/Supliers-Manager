80d079bdce64a35a0c3243ed18bcbbbc5041b52d
AndrewSkylone <andrewskylone@gmail.com>
2020-09-29 15:55:34 +0300

    self.__table_orders version

--- table/tableGUI.py
+++ table/tableGUI.py
@@ -11,18 +11,40 @@ class TableGUI(tk.Frame):
         self.table_height = height
         self.__table_orders = ()
         self.table = None
+        self.navigator = None
+        self.__listeners = []
 
         self.create_widgets()
     
     def create_widgets(self):
-        self.table = TreeView(master=self, height=self.table_height, show="headings")
+        self.table = TreeView(master=self, application=self, height=self.table_height, show="headings")
         self.table.grid(row=0, column=0)
 
+        # self.navigator = Navigator(master=self, )
+
+    def get_table_orders(self) -> list:
+        return list(copy.deepcopy(self.__table_orders))
+    
+    def set_table_orders(self, orders):
+        self.__table_orders = tuple(copy.deepcopy(orders))
+        self.on_table_orders_changed()
+
+    def on_orders_changed(self, orders):
+        self.set_table_orders(orders=orders)
+    
+    def subscribe(self, listener):
+        self.__listeners.append(listener)
+
+    def on_table_orders_changed(self):
+        for listener in self.__listeners:
+            listener.on_table_orders_changed(orders=self.get_table_orders())
+
 class TreeView(ttk.Treeview):
-    def __init__(self, master, height, **kw):
+    def __init__(self, master, application, **kw):
         ttk.Treeview.__init__(self, master, **kw)
 
-        self.height = height
+        self.app = application
+        self.page = 1
 
         self["columns"] = (0, 1, 2, 3)
         self.column(0, width=150)
@@ -35,29 +57,17 @@ class TreeView(ttk.Treeview):
         self.heading(2, text="MYXL number")
         self.heading(3, text="User")
 
-        for i in range(len(self.height)):
+        for i in range(self['height']):
             self.insert("", "end", iid=i, tags="all")
         self.tag_configure("all", font=("Arial", 11))
         # self.tag_bind("all", "<Return>", self.open_selected)
         # self.tag_bind("all", "<Button-1>", self.toggle_selected)
         # self.tag_bind("all", "<Control-c>", self.copy_value)
+
+    def show_page(self, orders, page):
+        pages = math.ceil(len(orders) / self["height"])
+
+    def clear(self):
+        for iid in range(self["height"]):
+            self.item(iid, value=[" "] * 4)
     
-    # def on_orders_changed(self, orders):
-    #     self.set_table_orders(orders=orders)
-
-    # def get_table_orders(self) -> list:
-    #     return list(copy.deepcopy(self.__table_orders))
-    
-    # def set_table_orders(self, orders):
-    #     self.__table_orders = tuple(copy.deepcopy(orders))
-    #     self.on_table_orders_changed()
-
-    # def on_table_orders_changed(self):
-    #     self.show_page(page=int(self.page_entry.get()))
-
-    # def show_page(self, page):
-    #     pages = math.ceil(len(stats) / self.table_height)
-
-    # def clear_table(self):
-    #     for iid in range(self.table["height"]):
-    #         self.table.item(iid, value=[" "] * 4)
